7b7230ec0000d966307732c35ad1c126eaf88b94
battlerager666 <155563645+battlerager666@users.noreply.github.com>
2024-01-04 15:06:47 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 var alleBuchstaben = Array("A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z");
 var wievielteRunde = 0;
-var timerMinutes;
-var timerSeconds;
+var ShowTimerMinutes;
+var timerSecondsTotal;
+var ShowTimerSeconds;
 let timerAktiv;
+
+function changeBackgroundColor(color){
+    document.body.style.background = color;
+}
 
 function neuesSpielStarten(){
     alleBuchstaben = new Array("A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"); //zurücksetzen
@@ -10,7 +15,7 @@ function neuesSpielStarten(){
     var übrigeBuchstabenZahlBestimmen = document.getElementById("p_Anzeige_übrigeBuchstaben"); //wieviele übrig? zurücksetzen
     übrigeBuchstabenZahlBestimmen.innerText = "Übrige Buchstaben: " + alleBuchstaben.length;
 
-    var ergebnisAnzeige = document.getElementById("p_Anzeige_ergebnis"); //buchstabenAnzeige zurücksetzen
+    var ergebnisAnzeige = document.getElementById("p_Anzeige_welcherBuchstabe"); //buchstabenAnzeige zurücksetzen
     ergebnisAnzeige.innerText = "...";
 
     wievielteRunde = 0;
@@ -26,7 +31,7 @@ function zufälligerBuchstabeAuslösen(){
     welcherIndex = alleBuchstaben.indexOf(zufälligerBuchstabe); //verwendeten Buchstaben entfernen
     alleBuchstaben.splice(welcherIndex, 1);
 
-    var ergebnisAnzeige = document.getElementById("p_Anzeige_ergebnis"); //den buchstaben anzeigen
+    var ergebnisAnzeige = document.getElementById("p_Anzeige_welcherBuchstabe"); //den buchstaben anzeigen
     ergebnisAnzeige.innerText = zufälligerBuchstabe;
 
     var übrigeBuchstabenZahlBestimmen = document.getElementById("p_Anzeige_übrigeBuchstaben"); //wieviele übrig? anzeigen
@@ -39,30 +44,77 @@ function zufälligerBuchstabeAuslösen(){
 }
 
 function StoppuhrAuslösen(){
+    changeBackgroundColor("white");
+
     if(document.getElementById("input_Stoppuhr_Zeit").value > 0){
-    timerMinutes = document.getElementById("input_Stoppuhr_Zeit").value; //timer Minuten
+    ShowTimerMinutes = document.getElementById("input_Stoppuhr_Zeit").value; //timer Minuten insgesamt
     } else{
-    timerMinutes = 2;
+    ShowTimerMinutes = 2;
     }
 
-    timerSeconds = timerMinutes * 60; //timer Sekunden
+    timerSecondsTotal = ShowTimerMinutes * 60; //timer Sekunden insgesamt
+
+    timerSecondsTotal++ //für die erste sekunde anzeigen
+    StoppuhrUpdaten();
 
     if(timerAktiv === undefined){
-    timerAktiv = setInterval(StoppuhrUpdaten, 1000); //timer aktivieren (nur einmal)
+    timerAktiv = setInterval(StoppuhrUpdaten, 1000); //timer aktivieren (kann nur einmal ausgeführt werden)
     }
 }
 
 function StoppuhrUpdaten(){
-    document.getElementById("p_Stoppuhr").innerText = timerMinutes + " : " + timerSeconds; //zeit anzeigen + timerSeconds--
-    timerSeconds--;
+    timerSecondsTotal--;
+    ShowTimerMinutes = Math.trunc(timerSecondsTotal / 60);
+    ShowTimerSeconds = timerSecondsTotal - (ShowTimerMinutes * 60); //showTimerMinutes und showTimerSeconds
+
+    if(ShowTimerMinutes <= 0 && ShowTimerSeconds <= 0){ //wenn beides = 0, reset
+        clearInterval(timerAktiv);
+        timerAktiv = undefined;
+        changeBackgroundColor("orange");
+    }
+
+    if(ShowTimerMinutes < 10){
+        ShowTimerMinutes = "0" + ShowTimerMinutes; //2:0 -> 02:00
+    }
+    if(ShowTimerSeconds < 10){
+        ShowTimerSeconds = "0" + ShowTimerSeconds;
+    }
+
+    document.getElementById("p_Anzeige_Stoppuhr").innerText = ShowTimerMinutes + " : " + ShowTimerSeconds; //zeit anzeigen
+
+    //console.log(ShowTimerMinutes, ShowTimerSeconds, timerSecondsTotal, timerAktiv); //testen
 }
 
-function StoppuhrAnhalten(){
-    clearInterval(timerAktiv); //mit button timer stoppen
+function StoppuhrBeenden(){ //alles resetten
+    clearInterval(timerAktiv);
 
-    timerAktiv = undefined; //reset
+    timerAktiv = undefined;
+
+    changeBackgroundColor("white");
+
+    zeitAnzeigen();
 }
 
-if(timerSeconds <= 0){
-    clearInterval(timerAktiv); //TEST
+function zeitAnzeigen(){
+    if(document.getElementById("input_Stoppuhr_Zeit").value > 0){
+        ShowTimerMinutes = document.getElementById("input_Stoppuhr_Zeit").value; //timer Minuten insgesamt
+        } else{
+        ShowTimerMinutes = 2;
+        }
+    
+    timerSecondsTotal = ShowTimerMinutes * 60; //timer Sekunden insgesamt
+
+    ShowTimerMinutes = Math.trunc(timerSecondsTotal / 60);
+    ShowTimerSeconds = timerSecondsTotal - (ShowTimerMinutes * 60); //showTimerMinutes und showTimerSeconds
+
+    if(ShowTimerMinutes < 10){
+        ShowTimerMinutes = "0" + ShowTimerMinutes; //2:0 -> 02:00
+    }
+    if(ShowTimerSeconds < 10){
+        ShowTimerSeconds = "0" + ShowTimerSeconds;
+    }
+    
+    document.getElementById("p_Anzeige_Stoppuhr").innerText = ShowTimerMinutes + " : " + ShowTimerSeconds; //zeit anzeigen
 }
+
+zeitAnzeigen();
